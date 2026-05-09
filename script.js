ab42a33ada9f60c14a71852ab9a75b55cdafdcb6
Kartik Gupta <kart964364@gmail.com>
2026-05-10 03:21:13 +0530

    Fix console log statement formatting

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Smooth scroll behavior for navigation links
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
